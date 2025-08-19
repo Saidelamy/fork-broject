0759c0a08cc980229dd372ebd62ae739894f1c3c
ahmed hatem <ahmedhatemomran@gmail.com>
2025-08-19 13:22:13 +0300

    Initialize

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navScrool = document.body.querySelector("#mainNav");
+
+    if (window.scrollY === 0) {
+      navScrool.classList.remove("navbar-shrink");
+    } else {
+      navScrool.classList.add("navbar-shrink");
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+
+  // Activate SimpleLightbox plugin for portfolio items
+  new SimpleLightbox({
+    elements: "#portfolio a.portfolio-box",
+  });
+});
